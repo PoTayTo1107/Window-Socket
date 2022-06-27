04aac9bef4e32ab68dcb6c94dc7166f4914298db
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2022-06-27 20:19:35 +0700

    login & signup GUI complete

--- client.py
+++ client.py
@@ -53,6 +53,22 @@ class Client:
 
         tk.mainloop()
 
+    def loginExe(self):
+        username = self.username.get()
+        password = self.password.get()
+        list = str(["Log in", username, password])
+        self.send(list)
+        out = self.receive()
+        if out == "0":
+            tkinter.messagebox.showinfo(
+                "Notification", "Log in successfully")
+            tk.destroy()
+            self.nickname = username
+            self.gui_loop()
+        else:
+            tkinter.messagebox.showerror(
+                "Notification", "Invalid username or password")
+
     def login_form(self):
         global tk
         tk = Tk()
@@ -67,19 +83,41 @@ class Client:
 
         self.password = Entry(tk, show='*')
         self.password.grid(column=1, row=1, pady=2)
-        self.username.bind('<Return>', lambda x: self.password.focus_set())
+        self.username.bind('<Return>', lambda: self.password.focus_set())
 
-        self.button = Button(tk, text=' Log in ', command=lambda:
-                             (self.send(self.username.get()),
-                              self.send(self.password.get()),
-                              tk.destroy()))
+        self.button = Button(tk, text=' Log in ',
+                             command=lambda: (self.loginExe()))
         self.button.grid(column=0, row=2, columnspan=2, pady=5)
-        self.password.bind('<Return>', lambda x: self.button.invoke())
+        self.password.bind('<Return>', lambda: self.button.invoke())
 
         tk.mainloop()
 
-        if self.creds:
-            self.send(self.creds)
+    def signupExe(self):
+        username = self.username.get()
+        password = self.password.get()
+        list = str(["Sign up", username, password])
+        self.send(list)
+        out = self.receive()
+        if out == "0":
+            tkinter.messagebox.showinfo(
+                "Notification", "Sign up successfully")
+            tk.destroy()
+            self.login_signup_picker()
+        elif out == "1":
+            tkinter.messagebox.showerror(
+                "Notification", "Username is too short")
+        elif out == "2":
+            tkinter.messagebox.showerror(
+                "Notification", "Password is too short")
+        elif out == "3":
+            tkinter.messagebox.showerror(
+                "Notification", "Username contains invalid character(s)")
+        elif out == "4":
+            tkinter.messagebox.showerror(
+                "Notification", "Username is already taken")
+        else:
+            tkinter.messagebox.showerror(
+                "Notification", "ERROR!")
 
     def signup_form(self):
         global tk
@@ -105,107 +143,72 @@ class Client:
         tk.mainloop()
 
     def gui_loop(self):
-        self.win = Tk()
-        self.win.configure(bg="lightgray")
+        global tk
+        tk = Tk()
+        tk.iconbitmap('imgs/notes.ico')
+        tk.title("E-note")
+        tk.config(bg='lightgray')
 
-        self.chat_label = Label(self.win, text='Chat: ', bg="lightgray")
+        self.chat_label = Label(tk, text='Chat: ', bg="lightgray")
         self.chat_label.config(state='disabled')
         self.chat_label.pack(padx=20, pady=5)
 
-        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area = tkinter.scrolledtext.ScrolledText(tk)
         self.text_area.config(font=("Arial", 12))
         self.text_area.pack(padx=20, pady=5)
 
-        self.msg_label = Label(self.win, text='Message: ', bg="lightgray")
+        self.msg_label = Label(tk, text='Message: ', bg="lightgray")
         self.msg_label.config(state='disabled')
         self.msg_label.pack(padx=20, pady=5)
 
-        self.input_area = Text(self.win, height=3)
+        self.input_area = Text(tk, height=3)
         self.input_area.pack(padx=20, pady=5)
 
-        self.send_button = Button(self.win, text='Send', command=self.write)
+        self.send_button = Button(tk, text='Send', command=self.write)
         self.send_button.config(font=("Arial", 12))
         self.send_button.pack(padx=20, pady=5)
 
         self.gui_done = True
 
-        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+        tk.protocol("WM_DELETE_WINDOW", self.stop)
 
-        self.win.mainloop()
+        tk.mainloop()
 
     def write(self):
         message = f"{self.nickname}: {self.input_area.get('1.0','end')}"
-        self.send(message)
+        print(message)
+        # self.send(message)
         self.input_area.delete()
 
     def stop(self):
         self.running = False
-        self.win.destroy()
+        tk.destroy()
         self.sock.close()
         exit(0)
 
-    def receiveGui(self):
-        while self.running:
-            try:
-                message = self.receive
-                if message == 'NICK':
-                    self.send(self.nickname)
-                else:
-                    if self.gui_done:
-                        self.text_area.config(state='normal')
-                        self.text_area.insert('end', message)
-                        self.text_area.yview('end')
-                        self.text_area.config(state='disabled')
-            except ConnectionAbortedError:
-                break
-            except:
-                self.sock.close()
-                break
+    # def receiveGui(self):
+    #     while self.running:
+    #         try:
+    #             message = self.receive()
+    #             if message == 'NICK':
+    #                 self.send(self.nickname)
+    #             else:
+    #                 if self.gui_done:
+    #                     self.text_area.config(state='normal')
+    #                     self.text_area.insert('end', message)
+    #                     self.text_area.yview('end')
+    #                     self.text_area.config(state='disabled')
+    #         except ConnectionAbortedError:
+    #             break
+    #         except:
+    #             self.sock.close()
+    #             break
 
     def send(self, msg):
         self.sock.send(msg.encode(FORMAT))
 
     def receive(self):
         return self.sock.recv(2048).decode(FORMAT)
-
-    def signupExe(self):
-        username = self.username.get()
-        password = self.password.get()
-        list = str(["Sign up", username, password])
-        self.send(list)
-        out = self.receive()
-        print(out)
-        if out == "0":
-            tkinter.messagebox.showinfo(
-                "Notification", "Sign up successfully")
-        elif out == "1":
-            tkinter.messagebox.showerror(
-                "Notification", "Username is too short")
-        elif out == "2":
-            tkinter.messagebox.showerror(
-                "Notification", "Password is too short")
-        elif out == "3":
-            tkinter.messagebox.showerror(
-                "Notification", "Username contains invalid character(s)")
-        elif out == "4":
-            tkinter.messagebox.showerror(
-                "Notification", "Username is already taken")
-        else:
-            tkinter.messagebox.showerror(
-                "Notification", "ERROR!")
-
-        tk.destroy()
-        self.login_signup_picker()
-
-    def sendNote(self):
-        print(self.receive)
-        self.send(input())
-        print(self.receive)
-        self.cont
-
-    def recvNote(self):
-        print(self.receive)
-        self.cont
 
 
 client = Client()
